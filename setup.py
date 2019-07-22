3b255ca302e453dfb0728de994d182247336f4a6
rhyspoulton <rhyspoulton92@gmail.com>
2019-07-22 05:23:24 +0000

    Added support for python 2.7 and ensure cython uses the correct version of python
    The cython now knows what version of python which the user is running it with so the cythonized code does not run into errors when importing the cythonized code.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize, build_ext
+import sys
 
 setup(
     name='VELOCIraptor_Python_Tools',
-    ext_modules = cythonize("velociraptor_python_tools_cython.pyx")
+    ext_modules = cythonize("velociraptor_python_tools_cython.pyx",language_level=sys.version_info[0])
 )
+
+if(sys.version_info[0]==2):
+    print("\033[93mDeprecationWarning:\033[0m This code was built with python 2 which will soon be no longer maintained, please consider moving to python 3")
